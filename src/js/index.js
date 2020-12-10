f291d6c036c764779bce811e966f8f5b4d387293
felix-oss <uv.mhjl@gmail.com>
2020-12-10 20:43:38 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -9,13 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(
-	<div className="container">
-		<Home />
-	</div>,
-	document.querySelector("#app")
-);
+ReactDOM.render(<Home />, document.querySelector("#app"));
